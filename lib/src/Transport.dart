d5e43aef5d36d7b999c8486b35bd98fd7af6a824
Gil Mendes <gil00mendes@gmail.com>
2015-09-09 00:56:44 +0100

    Add Transport layer class
    In the previous commit i forget to send the EventEmitter class

---
+++ lib/src/Transport.dart
@@ -0,0 +1,77 @@
+library connexa.transport;
+
+import 'dart:io';
+import 'EventEmiter.dart';
+import 'Parser.dart';
+
+enum TransportStates {
+  open,
+  closed,
+  closing
+}
+
+abstract class Transport extends EventEmitter {
+
+  TransportStates state = TransportStates.open;
+  HttpRequest request = null;
+
+  /**
+   * Construct
+   */
+  Transport(dynamic request) {
+    this._state = TransportStates.open;
+  }
+
+  void onRequest(HttpRequest request) {
+    this.request = request;
+  }
+
+  /**
+   * CLose the transport.s
+   */
+  void close(Function fn) {
+    if (this.state == TransportStates.closed ||
+        this.state == TransportStates.closing) {
+      return;
+    }
+
+    this.state = TransportStates.closing;
+    this.doClose(fn ?? () => null);
+  }
+
+  /**
+   * Called with a transport error.
+   *
+   * @param {String} message error
+   * @param {Object} error description
+   */
+  void onError(msg, desc) {
+    if (!this.events.get('error').isEmpty()) {
+      Error err = new Error();
+      err.msg = msg;
+      err.type = 'TransportError';
+      err.description = desc;
+      this.emit('error', err);
+    }
+  }
+
+  /**
+   * Called with parsed out a packets from the data stream.
+   */
+  void onPacket(var packet) {
+    this.emit('packet', packet);
+  }
+
+  /**
+   * Called with the encoded packet data.
+   */
+  void onData(Object data) {
+    this.onPacket(Parser.decode(data));
+  }
+
+  void onClose() {
+    this.state = TransportStates.closed;
+    this.emit('close');
+  }
+
+}
